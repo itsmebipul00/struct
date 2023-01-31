ff655f8f17d3d49b3eb2c7c662e2137231866cf4
bipul <itsmebipul00@gmail.com>
2023-01-31 18:42:55 +0530

    Add: uis

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
 import axios from "axios";
 import nasaImg from "./nasaImg.png";
 import { url } from "inspector";
+import {
+  useOnClickOutside,
+  useLockedBody,
+  useIntersectionObserver,
+} from "usehooks-ts";
+
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 // end date = today -2
 // start_date = today - 30
@@ -22,15 +30,28 @@ interface NasaDataType {
 type ModalPropType = {
   data: NasaDataType;
   setOpenModal: React.Dispatch<React.SetStateAction<NasaDataType | undefined>>;
+  setLocked: (locked: boolean) => void;
 };
 
-const Modal = ({ data, setOpenModal }: ModalPropType) => {
+const Modal = ({ data, setOpenModal, setLocked }: ModalPropType) => {
+  const ref = useRef(null);
+
+  const handleClickOutside = () => {
+    setOpenModal(undefined);
+    setLocked(false);
+  };
+
+  useOnClickOutside(ref, handleClickOutside);
+
   return (
-    <div className="fixed h-screen w-screen flex justify-center items-center top-0 left-0">
-      <div className="rounded-xl flex flex-col gap-4 brightness-100 bg-white text-black w-3/4">
+    <div className="fixed h-screen w-screen flex justify-center items-center top-0 left-0 bg-[#1E1E1E] bg-opacity-80">
+      <div
+        className="rounded-xl flex flex-col gap-4  bg-white text-black w-1/2"
+        ref={ref}
+      >
         <div
           className="p-2 text-right cursor-pointer ml-6"
-          onClick={() => setOpenModal(undefined)}
+          onClick={handleClickOutside}
         >
           X
         </div>
@@ -46,7 +67,7 @@ const Modal = ({ data, setOpenModal }: ModalPropType) => {
             <iframe src={data.hdurl} title={data.title} />
           )}
         </div>
-        <div className="p-2 flex flex-col gap-4">
+        <div className="py-3 px-6 flex flex-col gap-4">
           <div className="text-2xl font-semibold">{data.title}</div>
           <div className="font-light">{data.explanation}</div>
           <div className="font-light">
@@ -67,6 +88,11 @@ function App() {
   const [response, setResponse] = useState<NasaDataType[]>([]);
   const [featuredPost, setFeaturedPost] = useState<NasaDataType>();
   const [openModal, setOpenModal] = useState<NasaDataType>();
+  const [locked, setLocked] = useLockedBody(false, "root");
+  const intersectionRef = useRef<HTMLDivElement | null>(null);
+  const entry = useIntersectionObserver(intersectionRef, { rootMargin: "20%" });
+  const isVisible = !!entry?.isIntersecting;
+  const [loading, setLoading] = useState(false);
 
   const [startDate, setStartDate] = useState<string>(
     sDate.toISOString().split("T")[0]
@@ -79,24 +105,46 @@ function App() {
   useEffect(() => {
     const nasaService = async () => {
       try {
+        setLoading(true);
         const res = await axios.get(API);
+        console.log(res.data.length);
         if (res.data.length === 29) {
           setFeaturedPost(res.data[res.data.length - 1]);
           res.data.length = 28;
           setResponse(res.data.reverse());
         } else {
-          setResponse(res.data.reverse());
+          setResponse((prev) => [...prev, ...res.data.reverse()]);
         }
       } catch (error) {
         console.log(error);
+      } finally {
+        setLoading(false);
       }
     };
     nasaService();
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [startDate, endDate]);
 
   const handleModal = (data: NasaDataType) => {
     setOpenModal(data);
+    setLocked(true);
   };
+
+  useEffect(() => {
+    if (isVisible) {
+      console.log(startDate);
+      console.log(endDate);
+      let newStartDate = new Date(startDate) as string | Date;
+      let newEndDate = new Date(endDate) as string | Date;
+
+      (newEndDate as Date).setDate((newStartDate as Date).getDate() - 1);
+      (newStartDate as Date).setDate((newStartDate as Date).getDate() - 28);
+      newEndDate = (newEndDate as Date).toISOString().split("T")[0];
+      newStartDate = (newStartDate as Date).toISOString().split("T")[0];
+      setEndDate(newEndDate);
+      setStartDate(newStartDate);
+    }
+  }, [isVisible]);
 
   return (
     <div className="p-12 min-h-screen bg-[#1E1E1E] text-white flex flex-col gap-16">
@@ -131,7 +179,7 @@ function App() {
           )}
         </div>
       </div>
-      <div className="grid grid-cols-20 overflow-scroll gap-8">
+      <div className="grid grid-cols-20 overflow-scroll gap-8 min-h-screen">
         {response.map((data, idx) => {
           return (
             <div
@@ -161,7 +209,19 @@ function App() {
           );
         })}
       </div>
-      {!!openModal && <Modal data={openModal} setOpenModal={setOpenModal} />}
+      {!!openModal && (
+        <Modal
+          data={openModal}
+          setOpenModal={setOpenModal}
+          setLocked={setLocked}
+        />
+      )}
+      {true && (
+        <div className="[&>*]:flex [&>*]:gap-8">
+          <Skeleton count={4} height="15rem" width="20rem" inline={true} />
+        </div>
+      )}
+      <div ref={intersectionRef}></div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <SkeletonTheme baseColor="#1E1E1E" highlightColor="#282828">
     <App />
-  </React.StrictMode>
+  </SkeletonTheme>
 );
 
 // If you want to start measuring performance in your app, pass a function
